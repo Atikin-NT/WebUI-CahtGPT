9b98d67fab39bc683cd6b40b4d13dc59a1ce3c1f
AveZorgen <78862872+AveZorgen@users.noreply.github.com>
2023-05-09 11:19:30 +0300

    update chats

--- static/js/chat.js
+++ static/js/chat.js
@@ -1,3 +1,31 @@
+const updateHistory = () => {
+    newchathtml = `
+    <li><a href="/chat" class="flex py-3 px-3 items-center gap-3 relative rounded-md cursor-pointer break-all pr-14 bg-gray-800 hover:bg-gray-800 group animate-flash">
+    <div class="each-chat flex-grow-1 ms-3" style="display: flex; align-items: center;">
+        <i class="fa-solid fa-plus"></i>
+        <p>New chat</p>
+    </div>
+    </a></li>`
+    $("#history-list-group").empty().append(newchathtml);
+    $.ajax({
+        type: 'GET',
+        url: '/backend-api/conversations?offset=0&limit=20',
+        data: null,
+        success: function (data) {
+            let html_data = '';
+            for (let i = 0; i < data.total; i++) {
+                title = data.items[i].title;
+                html_data += `
+                <li><a href="#" onclick="getpopup(${data.items[i].id}, '${encodeURIComponent(title)}')" class="flex py-3 px-3 items-center gap-3 relative rounded-md cursor-pointer break-all pr-14 bg-gray-800 hover:bg-gray-800 group animate-flash">
+                <div class="each-chat flex-grow-1 ms-3"><i class="fa-solid fa-comment"></i><p>${data.items[i].title}</p></div>
+                </a></li>
+                `;
+            }
+            $("#history-list-group").append(html_data);
+        }
+     });
+}
+
 const create_message = (message, imageSrc) => {
     return `
     <a href="#" class="custom-msg list-group-item list-group-item-action d-flex gap-3 py-3">
@@ -33,6 +61,7 @@ $("#gpt-button").click(function() {
         success: function (data) {
             let gpt_data = create_message(data.answer, "static/images/gpt.png");
             $("#list-group").append(gpt_data);
+            updateHistory();
         }
     });
 });
